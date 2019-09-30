0bfcc5176b5c98b9660fefd12af2caf2cba36d6a
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-09-30 21:35:59 +0700

    tugas_04_golang
    okee

---
+++ tugas-04.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+  var mahasiswa = map[string]int{"aldo":182, "yosep":178}
+  fmt.Println("Aldo :", mahasiswa["aldo"],"cm")
+  fmt.Println("Yosep :", mahasiswa["yosep"],"cm")
+
+}
